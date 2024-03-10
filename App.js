e229266bec1db10ab3aa717953c875d38c1e0248
musaib-js <musaibaltafurahman@gmail.com>
2024-03-10 14:53:08 +0530

    application creation done.

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet} from 'react-native';
+import WebView from 'react-native-webview';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      source={{ uri: 'https://drshineclient.vercel.app/' }}
+      style={styles.container}
+    />
+    
   );
 }
 
