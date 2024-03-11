bdc4937b4d688fe78184f5ea2869bf9b4861bd8e
musaib-js <musaibaltafurahman@gmail.com>
2024-03-11 23:11:24 +0530

    changes done in assets and splash screen

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
-import { StyleSheet} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import { StyleSheet, Image} from 'react-native';
 import WebView from 'react-native-webview';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Load stuff
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+    prepare();
+  }
+  , []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }
+  , [appIsReady]);
+
+  if (!appIsReady) {
+    return (
+      <Image
+        source={require('./assets/splash.png')}
+        onLoadEnd={onLayoutRootView}
+        fadeDuration={0}
+        style={{ flex: 1, width: '100%', height: '100%' }}
+        ressizeMode="cover"
+      />
+    )
+  }
+
   return (
+    <SafeAreaProvider>
     <WebView
       source={{ uri: 'https://drshineclient.vercel.app/' }}
       style={styles.container}
+      onLayout={onLayoutRootView}
     />
-    
+    </SafeAreaProvider>
   );
 }
 
